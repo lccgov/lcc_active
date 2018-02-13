260fb44e473e77bf97fe7493890bc27b2b1a8f7f
Matt Williams <matt@oozoo.co.uk>
2018-02-13 13:48:42 +0000

    disable prompt for alpha sites

--- gulpfile.js
+++ gulpfile.js
@@ -299,7 +299,7 @@ gulp.task('prompt',['pre-flight'], function () {
 gulp.task('sp-upload', ['prompt'], (done) => {
     var glob = gutil.env.css ? 'dist/**/*.css' : gutil.env.js ? 'dist/**/*.js' : 'dist/**/*.*';
     return gulp.src(glob)
-    .pipe(((settings.siteUrl.indexOf("dev")) == -1 && (!noninteractive)) ? prompt.confirm({
+    .pipe(((settings.siteUrl.indexOf("dev") == -1) && (settings.siteUrl.indexOf("alpha") == -1) && (!noninteractive)) ? prompt.confirm({
         message: 'You appear to be deploying to a non dev environment, is that ok?',
         default: false
     }) : gutil.noop())
