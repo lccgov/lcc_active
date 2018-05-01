1ce76600f43ac84ea372df95fc590c7f4affae17
Nigel Smyth <Nigelsmyth@hotmail.com>
2018-05-01 14:29:41 +0100

    Changed the Join Today URL

--- app/assets/javascripts/active-behaviour.js
+++ app/assets/javascripts/active-behaviour.js
@@ -1,7 +1,7 @@
 (function ($) {
 
     //header
-    $('.header-right').append('<div class="col-xs-12 nav-btns"><a target="_blank" rel="external" href="http://sport.leeds.gov.uk/memberships/introduction.aspx" class="btn btn-lg pull-right btn-secondary nav-btn"><span class="sr-only">External link opens in new window </span>Join Today</a><a target="_blank" rel="external" href="https://sport.leeds.gov.uk/onlinebookings" class="btn btn-lg pull-right btn-main nav-btn"><span class="sr-only">External link opens in new window </span>Book Online</a></div>');
+    $('.header-right').append('<div class="col-xs-12 nav-btns"><a target="_blank" rel="external" href="http://sport.leeds.gov.uk/memberships" class="btn btn-lg pull-right btn-secondary nav-btn"><span class="sr-only">External link opens in new window </span>Join Today</a><a target="_blank" rel="external" href="https://sport.leeds.gov.uk/onlinebookings" class="btn btn-lg pull-right btn-main nav-btn"><span class="sr-only">External link opens in new window </span>Book Online</a></div>');
    
     //image gallery
     $("#gallery li img").hover(function () {
