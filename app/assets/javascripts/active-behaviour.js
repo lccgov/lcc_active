76a3a8e6bd1ad4102e01f9ece6917b0ab9105a84
Nigel Smyth <Nigelsmyth@hotmail.com>
2019-02-18 12:15:37 +0000

    Updated link to Join Today (correct link this time)

--- app/assets/javascripts/active-behaviour.js
+++ app/assets/javascripts/active-behaviour.js
@@ -1,8 +1,8 @@
 (function ($) {
 
     //header
-    $('.header-right').append('<div class="col-xs-12 nav-btns"><a target="_blank" rel="external" href="http://sport.leeds.gov.uk/memberships" class="btn btn-lg pull-right btn-secondary nav-btn"><span class="sr-only">External link opens in new window </span>Join Today</a><a target="_blank" rel="external" href="https://www.fastdd.co.uk/platinum/start.php?c=Active-Leeds" class="btn btn-lg pull-right btn-main nav-btn"><span class="sr-only">External link opens in new window </span>Book Online</a></div>');
-   
+    $('.header-right').append('<div class="col-xs-12 nav-btns"><a target="_blank" rel="external" href="https://www.fastdd.co.uk/platinum/start.php?c=Active-Leeds" class="btn btn-lg pull-right btn-secondary nav-btn"><span class="sr-only">External link opens in new window </span>Join Today</a><a target="_blank" rel="external" href="https://sport.leeds.gov.uk/onlinebookings" class="btn btn-lg pull-right btn-main nav-btn"><span class="sr-only">External link opens in new window </span>Book Online</a></div>');
+    
     //image gallery
     $("#gallery li img").hover(function () {
         $('#main-img').attr('src', $(this).attr('src'));
